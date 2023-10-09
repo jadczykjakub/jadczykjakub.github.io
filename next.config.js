683ec29370f5822b27c139252b9f2fa3bff39e6b
jadczykajakub <jadczyk.jakub@gmail.com>
2023-10-09 21:59:18 +0200

    deply fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         const fileLoaderRule = config.module.rules.find((rule) =>
           rule.test?.test?.('.svg'),
         )
-    
+
         config.module.rules.push(
           // Reapply the existing rule, but only for svg imports ending in ?url
           {
@@ -24,11 +24,13 @@ const nextConfig = {
     
         // Modify the file loader rule to ignore *.svg, since we have it handled now.
         fileLoaderRule.exclude = /\.svg$/i
+
+
     
         return config
       },
-      "output": "export",
-      "images": {
+      output: "export",
+      images: {
         "unoptimized": true
       },
 }
