31bf5494ce45aa1581694838695b12f64e56718a
jadczykajakub <jadczyk.jakub@gmail.com>
2023-10-09 22:06:49 +0200

    config again

--- next.config.js
+++ next.config.js
@@ -1,38 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule) =>
-          rule.test?.test?.('.svg'),
-        )
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test && rule.test.test && rule.test.test('.svg')
+    );
 
-        config.module.rules.push(
-          // Reapply the existing rule, but only for svg imports ending in ?url
-          {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/, // *.svg?url
-          },
-          // Convert all other *.svg imports to React components
-          {
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            resourceQuery: { not: /url/ }, // exclude if *.svg?url
-            use: ['@svgr/webpack'],
-          },
-        )
-    
-        // Modify the file loader rule to ignore *.svg, since we have it handled now.
-        fileLoaderRule.exclude = /\.svg$/i
+    // Clone the fileLoaderRule and modify it
+    const svgFileLoaderRule = { ...fileLoaderRule };
+    svgFileLoaderRule.test = /\.svg$/i;
+    svgFileLoaderRule.resourceQuery = /url/; // *.svg?url
 
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      svgFileLoaderRule,
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
 
-    
-        return config
-      },
-      output: "export",
-      images: {
-        "unoptimized": true
-      },
-}
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
