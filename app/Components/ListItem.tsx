cb45759d3123edb8630457d16601a38ea2f9153d
jadczykajakub <jadczyk.jakub@gmail.com>
2023-10-11 13:49:26 +0200

    fix mobile shrink

--- app/Components/ListItem.tsx
+++ app/Components/ListItem.tsx
@@ -4,7 +4,7 @@ import Arrow from '../Assets/svg/arrowRight.svg';
 export default function ListItem({ text }: { text: string }) {
   return (
     <li className='flex gap-2'>
-      <Arrow className={'w-2 mt-1 basis-auto'} />
+      <Arrow className={'w-2 mt-1 shrink-0'} />
       <span>{text}</span>
     </li>
   );
