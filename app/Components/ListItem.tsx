88e14f8d29ab994b4bdbd20fe0d8914c6c879d81
jadczykajakub <jadczyk.jakub@gmail.com>
2023-10-11 13:16:08 +0200

    fix mobile view

--- app/Components/ListItem.tsx
+++ app/Components/ListItem.tsx
@@ -3,8 +3,8 @@ import Arrow from '../Assets/svg/arrowRight.svg';
 
 export default function ListItem({ text }: { text: string }) {
   return (
-    <li className='flex gap-2 items-center'>
-      <Arrow className={'w-2'} />
+    <li className='flex gap-2'>
+      <Arrow className={'w-2 mt-1 basis-auto'} />
       <span>{text}</span>
     </li>
   );
